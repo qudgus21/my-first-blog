1f92bc6652b31ee233d9acad261226e46ea374a3
qudgus21 <47776356+qudgus21@users.noreply.github.com>
2019-07-20 08:00:54 +0900

    My Django Girls app, first commit

--- myenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\hwang byeong hyeon\desktop\djangogirls\myenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
